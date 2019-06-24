cc48436229e1452511a2d5fbdad318557d236c44
jdavidg1 <jdavidg_1@hotmail.com>
2019-06-24 16:27:12 -0500

    Inicialización de vectores, instanciación, uso foreach y temporizador

---
+++ ForeachYVectores/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;      //línea agregada para generar el timer
+
+namespace ForeachYVectores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Definicón de vector y varibales
+            
+            int[] numero;
+          
+            //instanciar el vector
+
+            numero = new int[5];
+
+            numero[0] = 5;
+            numero[2] = 2;
+
+            foreach(int elemento in numero)
+            {
+                Console.WriteLine(elemento);
+                Thread.Sleep(1000);  //Retado para que los escriba cada segundo los elementos
+            }
+            Console.ReadKey();
+        }
+    }
+}
